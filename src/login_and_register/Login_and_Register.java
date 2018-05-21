128e824a33ed93340cd168d19cbf2cdd6921fabe
Thilini-Weerasinghe <thiliniweerasinghe09@gmail.com>
2018-05-21 22:36:39 +0530

    this is a java login and regidter form

---
+++ src/login_and_register/Login_and_Register.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package login_and_register;
+
+/**
+ *
+ * @author Kasuni Gaveshika
+ */
+public class Login_and_Register {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
